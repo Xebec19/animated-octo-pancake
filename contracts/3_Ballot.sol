7ed69d519190f6ee907e7bc70a8e76405066022f
Xebec19 <rohant946@gmail.com>
2025-01-21 21:38:29 +0530

    wip Campaign contract

--- contracts/3_Ballot.sol
+++ contracts/3_Ballot.sol
@@ -1,19 +1,23 @@
   pragma solidity ^0.4.17;
 
-  contract Test {
-    uint[] public myArray;
+contract Campaign {
+  address public manager;
+  uint public minimumContribution;
+  address[] public approvers;
 
-    constructor() public {
-      myArray.push(1);
-      myArray.push(10);
-      myArray.push(30);
-    }
-
-    function getMyArray() public view returns (uint[]) {
-      return myArray;
-    }
-
-    function getArrayLength() public view returns(uint) {
-      return myArray.length;
-    }
+  struct Request {
+    string description;
+    
   }
+
+  constructor (uint minimum) public {
+    manager = msg.sender;
+    minimumContribution = minimum;
+  }
+
+  function contribute() public payable {
+    require(msg.value > minimumContribution);
+
+    approvers.push(msg.sender);
+  }
+}
