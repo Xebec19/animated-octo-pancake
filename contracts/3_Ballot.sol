09e9d0df15f6f1af6f2051e70d0dcc4c1638cb1e
Xebec19 <rohant946@gmail.com>
2025-01-23 22:40:37 +0530

    wip request

--- contracts/3_Ballot.sol
+++ contracts/3_Ballot.sol
@@ -1,23 +1,46 @@
   pragma solidity ^0.4.17;
 
 contract Campaign {
+  struct Request {
+    string description;
+    uint value;
+    address recipient;
+    bool complete;
+  }
+
   address public manager;
   uint public minimumContribution;
   address[] public approvers;
-
-  struct Request {
-    string description;
-    
-  }
+  Request[] public requests;
 
   constructor (uint minimum) public {
     manager = msg.sender;
     minimumContribution = minimum;
   }
 
+  modifier restricted() {
+    require(msg.sender == manager);
+    _;
+  }
+
+
   function contribute() public payable {
     require(msg.value > minimumContribution);
 
     approvers.push(msg.sender);
+  }
+
+  function createRequest(string description, uint value, address recipient)
+    public restricted {
+      Request storage newRequest = Request({
+        description: description,
+        value: value,
+        recipient: recipient,
+        complete: false
+      });
+
+      Request(description, value, recipient, false);
+
+      requests.push(newRequest);
   }
 }
