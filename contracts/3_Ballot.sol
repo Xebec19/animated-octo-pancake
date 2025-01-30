7125a4122d8ae6008d12303bc4ee082581809898
Xebec19 <rohant946@gmail.com>
2025-01-30 22:06:52 +0530

    wip approve requests

--- contracts/3_Ballot.sol
+++ contracts/3_Ballot.sol
@@ -44,4 +44,13 @@ contract Campaign {
 
       requests.push(newRequest);
   }
+
+  function approveRequest(uint index) public {
+    Request storage request = requests[index];
+    require(approvers[msg.sender]);
+    require(!requests[index].approvals[msg.sender]);
+
+    requests[index].approvals[msg.sender] = true;
+    requests[index].approvalCount++;
+  }
 }
